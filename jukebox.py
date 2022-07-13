0beafbaced3ac83838e2ea4de1bc96121fbdeffe
Bruno Ribeiro <brunorchaves1@gmail.com>
2022-07-13 17:10:46 -0300

    Update jukebox.py

--- jukebox.py
+++ jukebox.py
@@ -21,16 +21,20 @@ gpio.setup(13, gpio.IN) #tocar
 def tocarMusica(codigo):
     global musica_Tocando , parar
 
-    if(codigo == '1'):
+    if(codigo == 1):
+        print("musica ABBA")
         pygame.mixer.music.load('Abba_DancingQueen.mp3')
         pygame.mixer.music.play(0)
-    elif(codigo == '2'):
+    elif(codigo == 2):
+        print("musica CB")
         pygame.mixer.music.load('ChuckBerryJohnnyBGoode.mp3')
         pygame.mixer.music.play(0)
-    elif(codigo == '3'):
+    elif(codigo == 3):
+        print("musica AHA")
         pygame.mixer.music.load('ChuckBerryJohnnyBGoode.mp3')
         pygame.mixer.music.play(0)
-    elif(codigo == '4'):
+    elif(codigo == 4):
+        print("musica LZ")
         pygame.mixer.music.load('GoingToCalifornia.mp3')
         pygame.mixer.music.play(0)
     else:
@@ -41,31 +45,30 @@ tocando = 0
 while(1):
 
     start = time.time()
-    if( gpio.input(3) == gpio.HIGH & gpio.input(5) == gpio.LOW & 
-        gpio.input(7) == gpio.LOW & gpio.input(11) == gpio.LOW):
+    if( gpio.input(3) == gpio.HIGH and gpio.input(5) == gpio.LOW and 
+        gpio.input(7) == gpio.LOW and gpio.input(11) == gpio.LOW):
         codigo =1 
-    elif ( gpio.input(3) == gpio.LOW & gpio.input(5) == gpio.HIGH & 
-        gpio.input(7) == gpio.LOW & gpio.input(11) == gpio.LOW):
+    elif ( gpio.input(3) == gpio.LOW and gpio.input(5) == gpio.HIGH and
+        gpio.input(7) == gpio.LOW and gpio.input(11) == gpio.LOW):
         codigo =2 
-    elif ( gpio.input(3) == gpio.HIGH & gpio.input(5) == gpio.HIGH & 
-        gpio.input(7) == gpio.LOW & gpio.input(11) == gpio.LOW):
+    elif ( gpio.input(3) == gpio.HIGH and gpio.input(5) == gpio.HIGH and 
+        gpio.input(7) == gpio.LOW and gpio.input(11) == gpio.LOW):
         codigo =3 
-    elif ( gpio.input(3) == gpio.LOW & gpio.input(5) == gpio.LOW & 
-        gpio.input(7) == gpio.HIGH & gpio.input(11) == gpio.LOW):
+    elif ( gpio.input(3) == gpio.LOW and gpio.input(5) == gpio.LOW and
+        gpio.input(7) == gpio.HIGH and gpio.input(11) == gpio.LOW):
         codigo =4 
     else:
-        codigo = 4
+        codigo = 0
     if(gpio.input(13) == gpio.HIGH):
         
         if(tocando == 0):
-            t0 = threading.Thread(target = tocarMusica,args = (codigo, ))
-            # tocarMusica(codigo)
-            t0.start()
+            tocarMusica(codigo)
             print("tocando musica ")
             print(codigo)
             tocando = 1 
 
-    else:
+    elif(gpio.input(13) == gpio.LOW):
+
         pygame.mixer.music.stop()
         print("musica parada")
         tocando = 0
